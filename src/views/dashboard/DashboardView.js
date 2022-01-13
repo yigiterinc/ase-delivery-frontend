60212f4b4bc5f071a33914a81be98289b4a1c88c
Marton Szep <marton.szep@tum.de>
2022-01-13 13:24:10 +0100

    Added user interface to create, edit and delete entries in user, delivery, and box tables

--- src/views/dashboard/DashboardView.js
+++ src/views/dashboard/DashboardView.js
@@ -18,14 +18,15 @@ export const DashboardView = () => {
   const [showDelivery, setShowDelivery] = useState(false)
   const [showBox, setShowBox] = useState(false)
 
-  const userArray = {columns:[{dataField: 'ID', text:'ID'}, {dataField: 'Name', text:'Name'}, {dataField: 'Email', text:'Email'}, {dataField: 'Type', text:'Type'}, {dataField: 'Password', text:'Password'}, {dataField: 'RFID', text:'RFID'}],
+  // Replace with database data using the format below
+  let userArray = {columns:[{dataField: 'ID', text:'ID'}, {dataField: 'Name', text:'Name'}, {dataField: 'Email', text:'Email'}, {dataField: 'Type', text:'Type'}, {dataField: 'Password', text:'Password'}, {dataField: 'RFID', text:'RFID'}],
                   data: [{ID: "User1", Name: "UserName1", Email: "user@email.com", Type: "Dispatcher", Password: "Password", RFID: "RFIDToken"},
                         {ID: "User2", Name: "UserName2", Email: "user2@email.com", Type: "Deliverer", Password: "Password2", RFID: "RFIDToken2"}]
                   };
-  const deliveryArray = {columns:[{dataField: 'ID', text:'ID'}, {dataField: 'BoxID', text:'BoxID'}, {dataField: 'CustomerID', text:'CustomerID'}, {dataField: 'DelivererID', text:'DelivererID'}, {dataField: 'TrackingCode', text:'TrackingCode'}, {dataField: 'DeliveryStatus', text:'DeliveryStatus'}],
+  let deliveryArray = {columns:[{dataField: 'ID', text:'ID'}, {dataField: 'BoxID', text:'BoxID'}, {dataField: 'CustomerID', text:'CustomerID'}, {dataField: 'DelivererID', text:'DelivererID'}, {dataField: 'TrackingCode', text:'TrackingCode'}, {dataField: 'DeliveryStatus', text:'DeliveryStatus'}],
                       data: [{ID: "Delivery1", BoxID: "Box1", CustomerID: "Customer1", DelivererID: "Deliverer1", TrackingCode: "TrackingCode1", DeliveryStatus: "Status1"}]
                       };
-  const boxArray = {columns:[{dataField: 'ID', text:'ID'}, {dataField: 'Name', text:'Name'}, {dataField: 'Address', text:'Address'}],
+  let boxArray = {columns:[{dataField: 'ID', text:'ID'}, {dataField: 'Name', text:'Name'}, {dataField: 'Address', text:'Address'}],
                   data: [{ID: "Box1", Name: "BoxName1", Address: "BoxAddress1"}]
                   };
 
@@ -67,9 +68,9 @@ export const DashboardView = () => {
           </CDBSidebar>
         </div>
         <Col className="colHeight" >
-          { showUser ? <ElementListing title="Users" table={userArray}/> : null }
-          { showDelivery ? <ElementListing title="Deliveries" table={deliveryArray}/> : null }
-          { showBox ? <ElementListing title="Boxes" table={boxArray} /> : null }
+          { showUser ? <ElementListing title="Users" element='user' table={userArray} /> : null }
+          { showDelivery ? <ElementListing title="Deliveries" element='delivery' table={deliveryArray}/> : null }
+          { showBox ? <ElementListing title="Boxes" element='box' table={boxArray} /> : null }
         </Col>
       </Row>
     </Container>
