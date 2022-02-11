2b10091f35d012a30cd3522e39977cfbce70908a
Yigit Kemal Erinc <erincyigit@gmail.com>
2022-02-11 17:09:25 +0100

    Improve CreateDeliveryFrom UX

--- src/component/CreateDeliveryForm.js
+++ src/component/CreateDeliveryForm.js
@@ -1,6 +1,14 @@
-import React, { useState } from "react";
-import { TextField, Button } from "@material-ui/core";
-import { useDispatch } from "react-redux";
+import React, { useState, useEffect } from "react";
+import {
+  TextField,
+  Button,
+  MenuItem,
+  Select,
+  FormControl,
+  InputLabel,
+  Typography,
+} from "@material-ui/core";
+import { useDispatch, useSelector } from "react-redux";
 import { createDelivery, getDeliveries } from "../store/slices/deliverySlice";
 
 function CreateDeliveryForm(props) {
@@ -8,6 +16,14 @@ function CreateDeliveryForm(props) {
   const [createDeliveryBoxId, setCreateDeliveryBoxId] = useState("");
   const [createDeliveryDelivererId, setCreateDeliveryDelivererId] =
     useState("");
+
+  const [customerEmail, setCustomerEmail] = useState();
+  const [boxName, setBoxName] = useState();
+
+  const [delivererEmail, setDelivererEmail] = useState();
+
+  const users = useSelector((state) => state.user);
+  const boxes = useSelector((state) => state.box.boxes);
 
   const dispatch = useDispatch();
 
@@ -35,6 +51,96 @@ function CreateDeliveryForm(props) {
     props.setUpdatePerformed(true);
   };
 
+  const getUserEmailSelect = () => {
+    return (
+      <FormControl required variant="outlined">
+        <Typography style={{ fontSize: "18px" }}>Customer</Typography>
+        <Select
+          onChange={(e) => {
+            setCreateDeliveryCustomerId(e.target.value);
+            setCustomerEmail(
+              users.find((user) => user.id === e.target.value).email
+            );
+          }}
+          style={{
+            marginTop: "10px",
+            marginBottom: "20px",
+            minWidth: "20ch",
+          }}
+          value={customerEmail}
+        >
+          <MenuItem value={""}>
+            <em>None</em>
+          </MenuItem>
+          {users
+            .filter((user) => user.role === "CUSTOMER")
+            .map((user) => {
+              return <MenuItem value={user.id}>{user.email}</MenuItem>;
+            })}
+        </Select>
+      </FormControl>
+    );
+  };
+
+  const getDelivererEmailSelect = () => {
+    return (
+      <FormControl required variant="outlined">
+        <Typography style={{ fontSize: "18px" }}>Deliverer</Typography>
+        <Select
+          onChange={(e) => {
+            setCreateDeliveryDelivererId(e.target.value);
+            setDelivererEmail(
+              users.find((user) => user.id === e.target.value).email
+            );
+          }}
+          style={{
+            marginTop: "10px",
+            marginBottom: "20px",
+            minWidth: "20ch",
+          }}
+          value={delivererEmail}
+        >
+          <MenuItem value={""}>
+            <em>None</em>
+          </MenuItem>
+          {users
+            .filter((user) => user.role === "DELIVERER")
+            .map((user) => {
+              return <MenuItem value={user.id}>{user.email}</MenuItem>;
+            })}
+        </Select>
+      </FormControl>
+    );
+  };
+  const getBoxNameSelect = () => {
+    return (
+      <FormControl required variant="outlined">
+        <Typography style={{ fontSize: "18px" }}>Box Name</Typography>
+        <Select
+          onChange={(e) => {
+            setCreateDeliveryBoxId(e.target.value);
+            setBoxName(
+              boxes.find((box) => box.id === e.target.value).stationName
+            );
+          }}
+          style={{
+            marginTop: "10px",
+            marginBottom: "20px",
+            minWidth: "20ch",
+          }}
+          value={boxName}
+        >
+          <MenuItem value={""}>
+            <em>None</em>
+          </MenuItem>
+          {boxes.map((box) => {
+            return <MenuItem value={box.id}>{box.stationName}</MenuItem>;
+          })}
+        </Select>
+      </FormControl>
+    );
+  };
+
   return (
     <div
       style={{
@@ -46,41 +152,9 @@ function CreateDeliveryForm(props) {
         position: "relative",
       }}
     >
-      <TextField
-        required
-        id="outlined-required"
-        label="Customer ID"
-        style={{
-          marginBottom: "20px",
-          minWidth: "20ch",
-        }}
-        value={createDeliveryCustomerId}
-        onChange={(e) => setCreateDeliveryCustomerId(e.target.value)}
-      />
-
-      <TextField
-        required
-        id="outlined-required"
-        label="Deliverer ID"
-        style={{
-          marginBottom: "20px",
-          minWidth: "20ch",
-        }}
-        value={createDeliveryDelivererId}
-        onChange={(e) => setCreateDeliveryDelivererId(e.target.value)}
-      />
-
-      <TextField
-        required
-        id="outlined-required"
-        label="Box ID"
-        style={{
-          marginBottom: "20px",
-          minWidth: "20ch",
-        }}
-        value={createDeliveryBoxId}
-        onChange={(e) => setCreateDeliveryBoxId(e.target.value)}
-      />
+      {getUserEmailSelect()}
+      {getDelivererEmailSelect()}
+      {getBoxNameSelect()}
 
       <Button
         variant="contained"
